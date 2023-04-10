a6eaa21e9c2002df20c19bed27d423584481d038
UARM-DigArch <chris.prince@unilever.com>
2023-04-10 19:04:26 +0100

    auto_classification accession option added updated opex_manifest to match

--- auto_classification.py
+++ auto_classification.py
@@ -6,16 +6,51 @@ from datetime import datetime
 import argparse
 
 
-parser = argparse.ArgumentParser(description="OPEX Manifest Generator for Preservica Uploads")
-parser.add_argument('tree_root',nargs='?', default=os.getcwd())
-parser.add_argument("-p","--prefix",required=False, nargs='?')
-parser.add_argument("-rm","--empty",required=False,action='store_true')
-parser.add_argument("-acc","--accession",required=False,nargs='?')
-args = parser.parse_args()
+#lists need to be set globally. 
+listPath = []
+listRef = []
+listParent = []
+listType = []
+listLevel = []
+listRelPath = []
+listSize = []
+listCreateDate = []
+listModDate = []
+listAccessDate = []
+listExtension = []
+listBasename = []
+listAccession = []
+listNewRef = []
 
-output_filename = os.path.basename(args.tree_root) + "_AutoClass.xlsx"
+acc_count = 1
 
-path_root = os.path.dirname(args.tree_root)
+def main():
+    parser = argparse.ArgumentParser(description="OPEX Manifest Generator for Preservica Uploads")
+    parser.add_argument('tree_root',nargs='?', default=os.getcwd())
+    parser.add_argument("-p","--prefix",required=False, nargs='?')
+    parser.add_argument("-rm","--empty",required=False,action='store_true')
+    parser.add_argument("-acc","--accession",required=False,nargs='?')
+    parser.add_argument("-o","--output",required=False,nargs='?')
+    args = parser.parse_args()
+    
+    global output_path
+    if not args.output:
+        output_path = args.tree_root
+        print(f'No output path selected, defaulting to root Directory: {args.tree_root}')
+    else:
+        output_path = args.output
+        print(f'Output path set to: {output_path}')
+
+    #Blank Lists created
+    if args.empty:
+        print('Removing Empty Directories\n')
+        remove_empty_folders(args.tree_root)
+    
+    cwd = r"{}".format(args.tree_root)
+    df = auto_class(cwd,prefix=args.prefix,accession=args.accession)
+    export_xl(df,output_path)
+    print('Complete!')
+
 
 def accession_numbering(dir,prefix_accession):
     global acc_count 
@@ -23,26 +58,26 @@ def accession_numbering(dir,prefix_accession):
         accession_ref = prefix_accession + "-DIG-Dir"
     else:
         accession_ref = prefix_accession + "-DIG-" + str(acc_count)
-    acc_count += 1
+        acc_count += 1
     return accession_ref
 
-acc_count = 1
-
-def list_dirs(CWD,ROOTLEVEL):
+def list_dirs(CWD,ROOTLEVEL,accession=None):
     ref = 1
     try:
         listdir = os.listdir(CWD)
         #Sorts the list Alphabetically and Ignores: 1. Hidden Directories starting with '.', 2. '.opex' files, 3. Folders titled 'meta', 4. Script file 5. Output file. 
         listdir = sorted([f for f in listdir if not f.startswith('.') \
-                        and not f.endswith('.opex') and f != 'meta' \
+                        and not f.endswith('.opex') \
+                        and f != 'meta'\
                         and f != os.path.basename(__file__) \
-                        and f != os.path.basename(output_filename)])
+                        and not f.endswith('_AutoClass.xlsx') and not f.endswith('_autoclass.xlsx')\
+                        and not f.endswith('_EmptyDirectoriesRemoved.txt')])
         # Counts the number of Seperators in the current file.
         level = CWD.count(os.sep)
-        # Subracts the previous count from ROOTLEVEL to give the current level (For reference).
+        # Subracts the previous count from ROOTLEVEL to give the current level of the file (USed to compose the Archive reference).
         level = level-ROOTLEVEL
         for file in listdir:
-            # Processing and appending all the data to lists.
+            # Processing and appending the metadata to lists.
             dir = os.path.join(CWD,file)
             listPath.append(str(dir))
             rel_path = dir.replace(path_root,"")
@@ -60,15 +95,16 @@ def list_dirs(CWD,ROOTLEVEL):
             extension = os.path.splitext(dir)[1]
             listExtension.append(extension)
             listRef.append(ref)
+            #Optional Accessoin Reference - Currently the Archive Reference
+            if accession:
+                listAccession.append(accession_numbering(dir,accession))
             if os.path.isdir(dir):
                 type = "Dir"
                 listType.append(type)
-                list_dirs(dir,ROOTLEVEL)
+                list_dirs(dir,ROOTLEVEL,accession)
             else:
-                type = "Arc"
+                type = "File"
                 listType.append(type)
-            if args.accession:
-                listAccession.append(accession_numbering(dir,args.accession))
             ref = ref+1
 
     except Exception as e:
@@ -76,9 +112,9 @@ def list_dirs(CWD,ROOTLEVEL):
         print("Error Occured for directory/file: {}".format(listdir))
         pass
 
-# ref_loop will loop...
+# ref_loop function loops through the 
 
-def ref_loop(REF, PARENT, TRACK, LEVEL, NEWREF, df):
+def ref_loop(REF, PARENT, TRACK, LEVEL, df,NEWREF=None, PREFIX=None):
     # Indexes the Parent of a file against the Name (Giving the Loc of the Parent)
     idx = df.index[df['Name'] == PARENT]
     # If top-level has been reached, IE Index fails to match...
@@ -89,16 +125,17 @@ def ref_loop(REF, PARENT, TRACK, LEVEL, NEWREF, df):
         # If the Level is not 0, IE the ref-level has reached the top-most reference, NEWREF is NEWREF (The constructed Reference from the loop.)
         else:
             NEWREF = NEWREF
-        if args.prefix:
-            PREFIX_NEWREF = str(args.prefix) + "/" + NEWREF
+        if PREFIX:
+            PREFIX_NEWREF = str(PREFIX) + "/" + str(NEWREF)
             listNewRef.append(PREFIX_NEWREF)
         else:
+            PREFIX=None
             #NEWREF is appended to the NewRef list.
             listNewRef.append(NEWREF)
-    # Action if the top-level has not been reached. IE The loop, will iterate over itself.
-    # Parent Reference and Parent of Parent are retrieved to continue the loop.
+    # Action if the top-level has not been reached.
+    # Parent Reference and the Name of Parent (PARENTPARENT) are retrieved to continue the loop 
     else:
-        PARENTREF = df.loc[idx].Ref.item()
+        PARENTREF = df.loc[idx].Ref_Section.item()
         PARENTPARENT = df.loc[idx].Parent.item()
         # If TRACK is 1, IE the first iteration, it will append, REF 
         if TRACK == 1:
@@ -109,84 +146,79 @@ def ref_loop(REF, PARENT, TRACK, LEVEL, NEWREF, df):
         #PARENT of PARENT becomes PARENT, for lookup, this allows the loop to lookup the next file.  
         PARENT = PARENTPARENT
         TRACK = TRACK+1
-        #Loop iterates
-        ref_loop(REF, PARENT, TRACK, LEVEL, NEWREF, df)
+        #Loop iterates over itself - note that the loop is only being activated in else condition
+        # IE when the top-level HASN'T been reached. 
+        ref_loop(REF, PARENT, TRACK, LEVEL, df,NEWREF, PREFIX)
 
-def auto_class():
+def auto_class(cwd,prefix=None,accession=None):
     # ROOTLEVEL gives an intial count of the Root Directories Seperators.
     rootlevel = cwd.count(os.sep)
-    list_dirs(cwd,rootlevel)
+    global path_root
+    path_root = os.path.dirname(cwd)
+    list_dirs(cwd,rootlevel,accession)
 
-    # Creating the Dataframe.
-    # The Dataframe matches the Parent against it's Name. It then drops the additional columns as these are not necessary.  
+    # A Dataframe is created from the various lists appended in the list_dirs function.
+    # A merge on Parent agasint Name (Path) then occurs. Pulling through the Parent's details to that row. 
+    # Duplicate data dropped and / Parent_Ref requires data to be to be filled in..  
 
-    df = pd.DataFrame({'Level':listLevel,'Ref':listRef,'RelName': listRelPath, 'Name':listPath, \
+    df = pd.DataFrame({'RelName': listRelPath, 'Name':listPath, \
                     'Basename': listBasename,'Extension':listExtension,'Parent':listParent,\
                     'Att':listType,'Size':listSize,'CreateDate':listCreateDate,\
-                    'ModifiedDate': listModDate,'AccessDate':listAccessDate})
-    df = df.merge(df[['Name','Parent','Ref','RelName']],how='left',left_on='Parent',right_on='Name')
-    df = df.drop(['Name_y','Parent_y','RelName_y'], axis=1)
-    df = df.rename(columns={'Ref_x':'Ref','Ref_y':'PRef','Name_x':'Name','Parent_x':'Parent','RelName_x':'RelName'})
-    df['PRef'] = df['PRef'].fillna(0).astype(int)
-    df = df.astype({'PRef': int})
+                    'ModifiedDate': listModDate,'AccessDate':listAccessDate, \
+                    'Level':listLevel,'Ref_Section':listRef})
+    df = df.merge(df[['Name','Ref_Section']],how='left',left_on='Parent',right_on='Name')
+    df = df.drop(['Name_y'], axis=1)
+    df = df.rename(columns={'Ref_Section_x':'Ref_Section','Ref_Section_y':'Parent_Ref','Name_x':'Name'})
+    df['Parent_Ref'] = df['Parent_Ref'].fillna(0).astype(int)
+    df = df.astype({'Parent_Ref': int})
 
-    ListRef = df['Ref'].values.tolist()
+    #Lists of References, Parent and Levels are exported to lists for iterating in ref_loop
+    ListRef = df['Ref_Section'].values.tolist()
     ListParent = df['Parent'].values.tolist()
     ListLevel = df['Level'].values.tolist()
+    
+    #c is a count / total of items in ListRef, for a simple progress bar.
     c = 0
     tot = len(ListRef)
     for R,P,L in zip(ListRef,ListParent,ListLevel):
         c += 1
         print(f"Generating Auto Classification for: {c} / {tot}",end="\r")
+        # T is Track
         T = 1
-        #### Set NR to a different starting point.
-        NR = 1
-        ref_loop(R,P,T,L,NR,df)
-    df['NRef'] = listNewRef
-    if args.accession:
-        df['Accession Reference'] = listAccession
-
+        ref_loop(R,P,T,L,df,PREFIX=prefix)
+    df['Archive_Reference'] = listNewRef
+    if accession:
+        df['Accession_Reference'] = listAccession
     return df
 
-def export_xl(df):
-    df.to_excel(os.path.join(args.tree_root,output_filename))
+def export_xl(df,output_path):
+    if os.path.exists(output_path):
+        pass
+    else: os.makedirs(output_path)
+    output_filename = os.path.join(output_path,"meta",str(os.path.basename(output_path)) + "_AutoClass.xlsx")
+    with pd.ExcelWriter(output_filename,mode='w') as writer:
+        df.to_excel(writer)
 
-def remove_empty_folders(path_abs):
+def export_txt(list,output_path):
+    if os.path.exists(output_path):
+        pass
+    else: os.makedirs(output_path)
+    output_filename = os.path.join(output_path, "meta", str(os.path.basename(output_path)) + "_EmptyDirsRemoved.txt")
+    with open(output_filename,'w') as writer:
+        for line in list:
+            writer.write(f"{line}\n")
+
+def remove_empty_folders(remove_path):
     elist = []
-    walk = list(os.walk(path_abs))
+    walk = list(os.walk(remove_path))
     for path, _, _ in walk[::-1]:
         if len(os.listdir(path)) == 0:
             elist.append(path)
             os.rmdir(path)
     if elist: 
-        with open(os.path.join(args.tree_root, str(os.path.basename(args.tree_root)) + "_EmptyDirectoriesRemoved.txt"),'w') as writer:
-            for line in elist:
-                writer.write(f"{line}\n")
+        global output_path
+        export_txt(elist, output_path)
+    else: print('No directories removed!')
 
 if __name__ == "__main__":
-    
-    #Blank Lists created
-    listPath = []
-    listRef = []
-    listParent = []
-    listType = []
-    listLevel = []
-    listRelPath = []
-    listSize = []
-    listCreateDate = []
-    listModDate = []
-    listAccessDate = []
-    listExtension = []
-    listBasename = []
-    listAccession = []
-    listNewRef = []
-
-    acc_count = 1
-
-    if args.empty:
-        print('Removing Empty Directories\n')
-        remove_empty_folders(args.tree_root)
-    cwd = r"{}".format(args.tree_root)
-    df = auto_class()
-    export_xl(df)
-    print('Complete!')
+    main()
--- opex_manifest.py
+++ opex_manifest.py
@@ -6,13 +6,17 @@ import os
 import auto_classification as AC
 from sys import platform
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description="OPEX Manifest Generator for Preservica Uploads")
 parser.add_argument('folderpath',default=os.getcwd())
 parser.add_argument("-c","--autoclass",required=False,choices=['Catalog','Accession','Both'])
 parser.add_argument("-p","--prefix",required=False, nargs='+')
-parser.add_argument("-f","--fixity",required=False,action='store_true')
+parser.add_argument("-fx","--fixity",required=False,action='store_true')
 parser.add_argument("-rm","--empty",required=False,action='store_true')
+parser.add_argument("-f","--force",required=False,action='store_true')
+parser.add_argument("-o","--output",required=False,nargs='+')
+parser.add_argument("--clear-opex",required=False,action='store_true')
 args = parser.parse_args()
 
 print('Processing Directory: ' + args.folderpath)
@@ -27,15 +31,16 @@ if args.autoclass:
         
 if args.prefix:
     if args.autoclass == "Both":
-        if len(args.prefix) < 2: print('"Both" Option is selected, please pass two prefixes: [-p CATALOGPREFIX ACCPREFIX]'); raise SystemExit
+        print(len(args.prefix))
+        if len(args.prefix) < 2 or len(args.prefix) > 2: print('"Both" option is selected, please pass only two prefixes: [-p CATALOGPREFIX ACCPREFIX]'); raise SystemExit
         for n,a in enumerate(args.prefix):
-            if a.endswith("/"): a = str(a).removesuffix("/")
+            if a.endswith("/"): a = str(Path(a).with_suffix(""))
             else: 
                 if n == 0: prefix = a
                 else: prefixAcc = a
         print(f"Prefixes are set as, Catalog; {prefix}; Acc: {prefixAcc}")
     else:
-        for a in args.prefix: prefix = str(a).removesuffix("/")
+        for a in args.prefix: prefix = str(Path(a).with_suffix(""))
         print('Prefix is set as: ' + prefix)
     class_flag = True
 else: class_flag = False
@@ -64,32 +69,15 @@ elif alg == "SHA-256": hash = hashlib.sha256(); alglib = "sha256"
 elif alg == "SHA-512": hash = hashlib.sha512(); alglib = "sha512"
 else: hash = hashlib.sha1()
 
-###Reading Excel for Databases.
-
-#df = pd.read_excel(xl)
-#alg_l = []
-# fix_l = []
-# for i,row in df['FullName']:
-#     fix_l.append(HashGen(row))
-#     alg_l.append('SHA1')
-
-#print(os.listdir(root_dir))
-
 def HashGen(x):
     print(f'Generating Fixity for: {x}',end="\r") 
     with open(x,'rb') as f:
         d = hashlib.file_digest(f,alglib)
-        # while True:
-        #     d = f.read(BUF_SIZE)
-        #     if not d: break
     return d.hexdigest().upper()
-
-#print(HashGen(r"C:\Users\Chris.Prince\Downloads\Framework USLP Futures For Kees 07 08 18.pptx"))
 
 def WinPathCheck(path):
     if len(path) > 255 and sys.platform == "win32":
         path = "\\\\?\\" + path
-        #path = win32api.GetShortPathName(path)
     else: path = path
     return path
 
@@ -108,42 +96,37 @@ def FixityXMLGen(x):
 def OpexWriter(x,root):
     if x.endswith('.opex'): x = x
     else: x = str(x) + ".opex"
-    if os.path.exists(x):
-        print("File already exists... Avoiding Override")
+    if os.path.exists(x) and not args.force:
+        print("Opex exists. Avoiding Override")
         pass
     else:
         Opex = ET.tostring(root,pretty_print=True,xml_declaration=True,encoding="UTF-8",standalone=True)
         with open(f'{x}','w',encoding="UTF-8") as writer:
             writer.write(Opex.decode('UTF-8'))
-            print('Saved File to: ' + x)
+            if args.force and os.path.exists(x): print(f"Forcing Override to: {x}")
+            else: print('Saved File to: ' + x)
 
 def AutoClassicationIdentifier(x,df,identifier):
     idx = df.index[df['Name'] == x]
     if idx.empty:
-        ArcRef = prefix
+        ArcRef = "ERROR"
     else:
-        item = df.loc[idx].NRef.item()
-        ArcRef = prefix + "/" + item
+        ArcRef = df.loc[idx].Archive_Reference.item()
     identifier.text = ArcRef
     identifier.set("type","code")
 
-def AutoAccessionIdentifier(path,identifer):
+def AutoAccessionIdentifier(x,df,identifer):
     if args.autoclass == "Both":
         type = "AccRef"
     else:
         type = "code"
-    if not os.path.isdir(path):
-        global AccCount
-        if args.autoclass == "Both":
-            AccRef = str(prefixAcc) + "-DIG-" + str(AccCount)
-        else: AccRef = str(prefix) + "-DIG-" + str(AccCount)
-        AccCount += 1
+    idx = df.index[df['Name'] == x]
+    if idx.empty:
+        ArcRef = "ERROR"
     else:
-        AccRef = "Dir"
+        AccRef = df.loc[idx].Accession_Reference.item()
     identifer.text = AccRef
     identifer.set("type",type)
-    global AccList
-    AccList.append(AccRef)
 
 def AddPropertiesMetadata(x,root,df=None):
     try:
@@ -159,12 +142,12 @@ def AddPropertiesMetadata(x,root,df=None):
         if args.autoclass == "Catalog":
             AutoClassicationIdentifier(x,df,ident)
         elif args.autoclass == "Accession":
-            AutoAccessionIdentifier(x,ident)
+            AutoAccessionIdentifier(x,df,ident)
         elif args.autoclass == "Both":
             print('Running AutoClass')
             AutoClassicationIdentifier(x,df,ident)
             ident = ET.SubElement(idents,"{" + opex + "}Identifier")
-            AutoAccessionIdentifier(x,ident)
+            AutoAccessionIdentifier(x,df,ident)
         else: print('Unknown Option... I dunno what\'s goin\' on?')
     except Exception as e:
         print(e)
@@ -192,7 +175,7 @@ def FixityIdentifierOpex(x,df):
     except Exception as e:
         print(e)
 
-def ManifestDirs(x, c):
+def manifest_dirs(x, c):
     os.chdir(x)
     rpath = os.getcwd()
     root = ET.Element("{" + opex + "}OPEXMetadata",nsmap={"opex":"http://www.openpreservationexchange.org/opex/v1.2"})
@@ -203,10 +186,13 @@ def ManifestDirs(x, c):
     if class_flag: 
         AddPropertiesMetadata(x, root, df)
 
-    #First For Loop is Create File Level Opex's, if Fixity or Auto-Classification options are selected. If no Options are selected, this section is ignored.
+    #1st For Loop is Create File Level Opex's, if Fixity or Auto-Classification options are selected. If no options are selected, no action is taken in loop.
      
     for f in os.listdir(x):
-        if not f.startswith('.') and not f.endswith('_AutoClass.xlsx') and not f.endswith('_Fixities.txt') and not f.endswith('_EmptyDirectoriesRemoved.txt'):
+        if not f.startswith('.') and not f.endswith('_AutoClass.xlsx') \
+            and not f.endswith('_Fixities.txt') \
+            and not f.endswith('_EmptyDirsRemoved.txt')\
+            and not f == 'meta':
             Path = os.path.join(rpath,f)
             Path = WinPathCheck(Path)
             if os.path.isdir(Path): pass
@@ -216,11 +202,14 @@ def ManifestDirs(x, c):
                 elif fix_flag: FixityOpex(Path)
                 else: pass
     
-    #Second For Loop is to Create Folder Level Manifests' of the folders.
+    #2nd For Loop is to create folder level manifests.
 
     for f in os.listdir(x):
         print(f"Generating Manifest: {c}",end="\r")
-        if not f.startswith('.') and not f.endswith('_AutoClass.xlsx') and not f.endswith('_Fixities.txt') and not f.endswith('_EmptyDirectoriesRemoved.txt'):
+        if not f.startswith('.') and not f.endswith('_AutoClass.xlsx') \
+            and not f.endswith('_Fixities.txt')\
+            and not f.endswith('_EmptyDirsRemoved.txt')\
+            and not f == 'meta':
             Path = os.path.join(rpath,f)
             Path = WinPathCheck(Path)
             if os.path.isdir(Path):
@@ -233,18 +222,18 @@ def ManifestDirs(x, c):
                     file.set("type","content")
                     file.set("size",str(os.path.getsize(Path)))
                 file.text = str(f)
+        else: pass
+
     c += 1
     Path = f'{os.path.join(x, os.path.basename(x))}.opex'
     Path = WinPathCheck(Path)
     OpexWriter(Path, root)
-
+    
     #3rd For Loop is iterate recurively through folders.
-
     for f in os.listdir(x):
-        c += 1
-        if not f.startswith('.'):
+        if not f.startswith('.') and not f == 'meta':
             Path = os.path.join(rpath,f)
-            if os.path.isdir(Path): ManifestDirs(Path, c)
+            if os.path.isdir(Path): manifest_dirs(Path, c)
 
 PathList = []
 FixityList = []
@@ -256,29 +245,22 @@ if __name__ == "__main__":
     if empty_flag:
         AC.remove_empty_folders(root_dir)
     if args.autoclass == "Catalog" :
-        df = AC.AutoClass(root_dir,prefix)
-        AC.ExportXL(df)
-        AccCount = None
+        df = AC.auto_class(root_dir,prefix=prefix)
     elif args.autoclass == "Accession":
-        df = None
-        AccCount = 1
-        AccList = []
+        df = AC.auto_class(root_dir,accession=prefix)
     elif args.autoclass == "Both":
-        df = AC.AutoClass(root_dir)
-        AC.ExportXL(df)
-        AccCount = 1
-        AccList = []
-    else: AccCount = None
-    ManifestDirs(root_dir, 1)
+        df = AC.auto_class(root_dir,prefix=prefix,accession=prefixAcc)
+    else: pass
+    if args.output: output_path = args.output
+    else: output_path = root_dir
+    if args.autoclass:
+        AC.export_xl(df,output_path)
+    manifest_dirs(root_dir, 1)
     if fix_flag:
-        os.makedirs("../meta")
-        with open("../meta/" + os.path.basename(root_dir) + "_Fixities.txt", 'w',encoding="UTF-8") as writer:
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        output_filename = os.path.join(output_path,"meta", os.path.basename(root_dir) + "_Fixities.txt", )
+        with open(output_filename,'w',encoding="UTF-8") as writer:
             for (path,fixity) in zip(PathList,FixityList):
                 writer.write("{0};{1}\n".format(path,fixity))
-    if args.autoclass == "Both" or args.autoclass == "Accession":
-        with open("../meta/" + os.path.basename(root_dir) + "_AccessionList.txt", 'w', encoding="UTF-8") as writer:
-            for (path,AccRef) in zip(PathList, AccList):
-                print(AccRef)
-                writer.write("{0};{1}\n".format(path,AccRef))
-
     print('\nComplete!')
