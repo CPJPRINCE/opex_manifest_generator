d491d764372e40d92df53d5c85e07d17a5d74378
CPJPRINCE <c.pj.prince@gmail.com>
2024-02-19 14:20:20 +0000

    init mod

--- opex_manifest_generator/__init__.py
+++ opex_manifest_generator/__init__.py
@@ -11,5 +11,5 @@ from .common import *
 from .cli import parse_args,run_cli
 
 __author__ = "Christopher Prince (c.pj.prince@gmail.com)"
-__version__ = "1.0"
+__version__ = "1.0.5"
 __license__ = "Apache License Version 2.0"
