0da346b2f5e3e314416c8b1ea7f43e749b440c08
CPJPRINCE <c.pj.prince@gmail.com>
2024-04-21 19:37:50 +0100

    changed version lookup

--- opex_manifest_generator/__init__.py
+++ opex_manifest_generator/__init__.py
@@ -9,7 +9,5 @@ from .opex_manifest import OpexManifestGenerator,OpexDir,OpexFile
 from .hash import HashGenerator
 from .common import *
 from .cli import parse_args,run_cli
-from .version import __version__
 __author__ = "Christopher Prince (c.pj.prince@gmail.com)"
-__version__ = __version__
 __license__ = "Apache License Version 2.0"
