997250f6c5b9cbda4990661bef55fe6554bdeccc
CPJPRINCE <c.pj.prince@gmail.com>
2026-01-22 22:03:20 +0000

    Added logger! And converted prints to logging can use --log-level option to set log levels and --log-file to output logs to file  --prevent-all-exports: prevents exports of output files (not logs) --empty-export: prevents export when using --remove-empty --removal-export: prevents export when using removals column --convert-xmls: converts current xmls placed in mdir folder to spreadsheets. --zip-remove-files - removes files after zipping
    Also:
    -fixed a bug when using input option.
    -added more specific exceptions
    -more tests with copilot.
    -changed from global constants to self
    -updated  typing
    added pytes

--- opex_manifest_generator/common.py
+++ opex_manifest_generator/common.py
@@ -5,40 +5,51 @@ author: Christopher Prince
 license: Apache License 2.0"
 """
 
-import zipfile, os, sys, time, stat, datetime, shutil
-from lxml import etree
+import zipfile, os, sys, stat, shutil, logging, lxml
+from datetime import datetime, timedelta
 
-def zip_opex(file_path,opex_path):
+logger = logging.getLogger(__name__)
+
+def zip_opex(file_path,opex_path = None) -> str:
     zip_file = f"{file_path}.zip"
     if not os.path.exists(zip_file):
         with zipfile.ZipFile(zip_file,'w') as z:
-            z.write(file_path,os.path.basename(file_path))
-            z.write(opex_path,os.path.basename(opex_path))
-    else: print(f'A zip file already exists for: {zip_file}')
+            if os.path.exists(file_path):
+                z.write(file_path,os.path.basename(file_path))
+            if opex_path is not None and os.path.exists(opex_path):
+                z.write(opex_path,os.path.basename(opex_path))
+        logger.debug(f'File has been zipped to: {zip_file}')
+    else:
+        logger.warning(f'A Zip file already exists for: {zip_file}')
+    return zip_file
 
-def remove_tree(path: str, removed_list: list):
+def remove_tree(path: str, removed_list: list) -> None:
     removed_list.append(path)
-    print(f"Removing: {path}")
+    logger.info(f"Removing: {path}")
     if os.path.isdir(path):
         for dp,d,f in os.walk(path):
             for fn in f:
                 removed_list.append(win_256_check(dp+win_path_delimiter()+fn))
+                logger.info(f'Removing {dp + win_path_delimiter() + fn}')            
             for dn in d:
                 removed_list.append(win_256_check(dp+win_path_delimiter()+dn))
+                logger.info(f'Removing {dp + win_path_delimiter() + dn}')
         shutil.rmtree(path)
     else:
         if os.path.exists(path):
             os.remove(path)
+            logger.info (f'Removing File: {path}')
 
-def win_256_check(path):
+def win_256_check(path) -> str:
     if len(path) > 255 and sys.platform == "win32":
+        logger.debug(f'Path: {path} is greater than 255 Characters')
         if path.startswith(u"\\\\?\\"):
             path = path 
         else:
             path = u"\\\\?\\" + path
     return path
 
-def filter_win_hidden(path: str):
+def filter_win_hidden(path: str) -> bool:
     if sys.platform =="win32":
         if bool(os.stat(path).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN) is True:
             return True
@@ -47,33 +58,33 @@ def filter_win_hidden(path: str):
     else:
         return False
     
-def win_path_delimiter():
+def win_path_delimiter() -> str:
     if sys.platform == "win32":
         return "\\"
     else:
         return "/"
 
-def check_nan(value):
+def check_nan(value) -> str | None:
     if str(value).lower() in {"nan","nat"}:
         value = None
     return value
 
-def check_opex(opex_path:str):
+def check_opex(opex_path:str) -> bool:
     opex_path = opex_path + ".opex" 
     if os.path.exists(win_256_check(opex_path)):
         return False
     else: 
         return True
 
-def write_opex(path: str, opexxml: etree.Element):
+def write_opex(path: str, opexxml: lxml.etree.Element) -> str:
     opex_path = win_256_check(str(path) + ".opex")
-    opex = etree.indent(opexxml, "  ")
-    opex = etree.tostring(opexxml, pretty_print=True, xml_declaration=True, encoding="UTF-8", standalone=True)
+    opex = lxml.etree.indent(opexxml, "  ")
+    opex = lxml.etree.tostring(opexxml, pretty_print=True, xml_declaration=True, encoding="UTF-8", standalone=True)
     with open(f'{opex_path}', 'w', encoding="UTF-8") as writer:
         writer.write(opex.decode('UTF-8'))
-        print('Saved Opex File to: ' + opex_path)
+        logger.info('Saved Opex File to: ' + opex_path)
     return opex_path
 
-def print_running_time(start_time):
-    print(f'Running time: {datetime.datetime.now() - start_time}')
-    time.sleep(5)
+def running_time(start_time) -> timedelta:
+    running_time = datetime.now() - start_time 
+    return running_time
